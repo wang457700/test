d318cf88e617c9137c8ba9b5a45aa32bbb70cff9
linjiahong <572580083@qq.com>
2018-08-23 09:16:51 +0800

    Default

--- application/home/common/function.php
+++ application/home/common/function.php
@@ -13,13 +13,3 @@
 function test(){
     echo 1;
 }
-
-
-/**
- * 根据幻灯片标识获取所有幻灯片
- * @param string $slide 幻灯片标识
- * @return array;
- */
-function sp_getslide(){
-    return Db::name('slide')->where('id',1)->find();
-}
