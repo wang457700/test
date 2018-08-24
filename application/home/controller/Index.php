a23311033d81f29cc8ad1e1ccaaf9b53f1eda97d
xiaogang <457700516@qq.com>
2018-08-24 14:07:20 +0800

    非官方

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -22,7 +22,17 @@ class Index extends Frontend
     public function index()
     {
 
+
         $slide =  Db::name('slide')->where(array('slide_status'=>1,))->select();
+        $top_ten= Db::name('goods')->where('is_best_status',0)->limit(10)->select();
+        $product_list= Db::name('goods')->where('cat_id',15)->limit(9)->select();
+        $health_food= Db::name('goods')->where('cat_id',16)->limit(9)->select();
+
+        $share= Db::name('user_share')->limit(4)->select();
+        $this->view->assign("share", $share); //復康產品
+        $this->view->assign("product_list", $product_list); //復康產品
+        $this->view->assign("top_ten", $top_ten);   //Top Ten
+        $this->view->assign("health_food", $health_food);   //健康及有機產品
         $this->view->assign("slide", $slide);
         return $this->view->fetch();
     }
@@ -35,10 +45,6 @@ class Index extends Frontend
         $this->view->assign("title", $page['post_title']);
         return $this->view->fetch();
     }
-
-
-
-
 
 
 }
