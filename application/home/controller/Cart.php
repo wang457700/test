bf8a8fbf5bc9cd3b9f2fc39cc015f7755975f1ff
xiaogang <457700516@qq.com>
2018-08-24 18:32:37 +0800

    购物车

--- application/home/controller/Cart.php
+++ application/home/controller/Cart.php
@@ -41,14 +41,60 @@ class Cart extends Frontend
 
     }
 
+    public function action_cart(){
+
+         $goods_id= input('goods_id/a');
+        $goods_num= input('goods_num/a');
+        if (count($goods_id) > 1) {
+            $count=count($goods_id);
+            $i = 0;
+            $data = array();
+            for ($i = 0; $i < $count; $i++) {
+                $data[$i]['goods_id'] = $goods_id[$i];
+                $data[$i]['goods_num'] = $goods_num[$i];
+            }
+            $res='';
+            foreach ($data as $k => $v) {
+                     $price=Db::name('goods')->where('product_id',$v['goods_id'])->find();
+                    $fat['price'] =$price['price'];
+                    $fat['money_total']=$v['goods_num']*$price['price'];
+                    $fat['goods_sn'] =$price['freight_num'];
+                    $fat['order_sn'] =date('Y-m-d').time();
+                    $fat['goods_id'] = $v['goods_id'];//这里都要加上下标
+                    $fat['goods_num'] = $v['goods_num'];//这里都要加上下标
+                    $fat['user_id'] = Session::get('user_id');
+                    $fat['addtime'] = date('Y-m-d H:i:s',time());
+                    if ($v != "") {
+                        $res= db("order")->insert($fat);
+                    }
+            }
+
+            if ($res) {
+                $data = array(
+                    'code' => 1,
+                    'msg' => '你已经生成订单！',
+                );
+                $this->ajaxReturn($data);
+            }else{
+                $data = array(
+                    'code' => 0,
+                    'msg' => '订单生成失败！',
+
+                );
+                $this->ajaxReturn($data);
+            }
+
+        }
+
+    }
+
+
+
     public function shopping_cart(){
 
 
         $user_id=  Session::get('user_id');
-        $list= Db::name('cart_order')->alias('a')->field('c.*,a.product_id as goods_id,a.user_id,a.cart_id')->join('__GOODS__ c','a.product_id=c.product_id','RIGHT')->where('user_id',8)->group('c.product_id')->select();
-
-
-
+        $list= Db::name('cart_order')->alias('a')->field('c.*,a.product_id as goods_id,a.user_id,a.cart_id')->join('__GOODS__ c','a.product_id=c.product_id','RIGHT')->where('user_id', $user_id)->group('c.product_id')->select();
         $sum=0;
         foreach ($list as $key=>$item){
 
@@ -56,7 +102,6 @@ class Cart extends Frontend
                $list[$key]['total']=$total;
                $list[$key]['total_price']=$total*$item['price'];
                $sum+=$list[$key]['total_price'];
-
         }
 
         $this->assign('all_price',$sum);
