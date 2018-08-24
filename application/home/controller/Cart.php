c007861b67f54393891a48cb8d5e54b392fd918f
xiaogang <457700516@qq.com>
2018-08-24 15:11:13 +0800

    购物车

--- application/home/controller/Cart.php
+++ application/home/controller/Cart.php
@@ -8,16 +8,35 @@
 
 namespace app\home\controller;
 use app\common\controller\Frontend;
-
+use EasyWeChat\Staff\Session;
+use think\Db;
+use think\Session;
 class Cart extends Frontend
 {
 
 
 
-    public function checkin_cart(){
+    public function checkin_cart()
+    {
+
+        $data['product_id'] = input('product_id');
+        $data['user_id'] = session('user_id');
+        $data['add_time'] = date('Y-m-d H:i:s');
+        $res = Db::name('cart_order')->insertGetId($data);
+        $total = Db::name('cart_order')->where('user_id',session('user_id'))->count();
+        if ($res) {
+            $data = array(
+                'code' => 1,
+                'msg' => '加入购物车成功！',
+                'total' => $total
+            );
+     $this->ajaxReturn($data);
+        }
+
+    }
 
 
-
-
+    public function ajaxReturn($data,$type = 'json'){
+        exit(json_encode($data));
     }
 }
