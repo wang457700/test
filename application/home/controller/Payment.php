dbefe623097217ac8291b64f8ebf4131975a37e8
linjiahong <572580083@qq.com>
2018-08-25 21:21:17 +0800

    linjiahong

--- application/home/controller/Payment.php
+++ application/home/controller/Payment.php
@@ -16,20 +16,19 @@ class Payment extends  Frontend
 
     public function go_pay(){
 
-            $order_sn=base64_decode(input('order_sn'));
+        $order_sn=base64_decode(input('order_sn'));
 
-           $order_list= Db::name('order');$order_list->where(array('user_id'=>Session::get('user_id'),'order_sn'=>$order_sn))->select();
+        $order_list= Db::name('order')->where(array('user_id'=>Session::get('user_id'),'order_sn'=>$order_sn))->find();
 
-
-
-        $this->assign('order_sn',$order_list);
+        dump($order_list);
+        $this->assign('order_list',$order_list);
         $this->assign('title','支付订单');
        return $this->fetch();
     }
 
     public function Payment_done(){
 
-        $order_sn=input('order_sn');
+        $order_sn=base64_decode(input('order_sn'));
         $this->assign('order_sn',$order_sn);
         $this->assign('title','完成支付');
         return $this->fetch();
